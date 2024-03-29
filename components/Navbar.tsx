86006f9156acaa9ebe0a03d5e4b91df0224c6a19
amala <amalakkad93@outlook.com>
2024-03-28 21:52:01 -0700

    working on auth

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 
 const Navbar = () => {
-  const session = {};
+  const session = null;
   return (
     <nav className="flexBetween navbar">
       <div className="flex-1 flexStart gap-10">
